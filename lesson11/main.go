19d191f47141b612cdbd9dd1be0d0555d3d7d5f3
autistiq <mustrumridcullyy@gmail.com>
2026-09-21 02:02:44 +0300

    sprint 2: day 6: decoded caesar cipher via runes

--- lesson11/main.go
+++ lesson11/main.go
@@ -1,6 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
 	message := "L fdph, L vdz, L frqtxhuhg"
-
+	runes := []rune(message)
+	for i := 0; i < len(runes); i++ {
+		c := runes[i]
+		if (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') {
+			c = c - 3
+		}
+		fmt.Printf("%c", c)
+	}
 }
